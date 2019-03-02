83d769c18cf952e6ada40f90c35792e85d7f2d66
James Moore <jmoore17@luc.edu>
2019-03-02 12:36:23 -0600

    Uploading example script on how to convert a csv file into JSON format

---
+++ csvToJson_script.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 25 09:52:01 2019
+
+@author: James Moore
+"""
+#iteration 1: 1/25/19 --> Lean Solution
+#iteration 2: 1/26/19 --> Robust exceptions added
+#iteration 3: 1/28/19 --> Solution changed to utilize pandas methods
+import json
+import os
+import sys
+import pandas as pd
+
+#Define Function
+
+
+#Define Function
+
+#specify directory
+wkdir = 'NA'
+csvFilename = 'NA'
+numLoop = 0
+while os.path.isdir(wkdir)==False or os.path.isfile(csvFilename)==False:
+    #specify directory of file to convert
+    wkdir = str(input('Please Specify Directory: '))     
+    if os.path.isdir(wkdir)==False:
+        print('Invalid Directory Specified:\n')
+        numLoop = numLoop+1
+    #specify name of file to convert
+    else:
+        os.chdir(wkdir)
+        csvFilename = str(input('Please Specify File Name: '))
+        
+        #Check if the user does not enter a .csv specification
+        if csvFilename[-4:] != '.csv':
+            csvFilename = csvFilename+'.csv'
+        
+        #check if file exists
+        if os.path.isfile(csvFilename)==False:
+            print('\nInvalid Input:\n' \
+                  'The file entered does not exist in the specified directory.\n' \
+                  'Please Check Directory and File Specifications and Try Again.\n')
+            numLoop = numLoop+1
+    
+    #terminate after 3 failed attempts to find existing file        
+    if numLoop > 3:
+        sys.exit('Too Many Failed Attempts\n' \
+                 'Program Terminating...')
+#%%  
+#Read in .csv data using pandas
+csvReader = pd.read_csv(csvFilename)
+
+#create blank list to write to .json
+callList = list()
+
+#get unique call-ids
+callID = csvReader[csvReader.columns[0]].unique()
+
+#iterate through unique call-ids
+for idKey in callID:
+    #set up nested list
+    nestIDList = ('call-id','start','end','users')
+    idNest = dict([(key,[]) for key in nestIDList])
+    
+    #subset based on call-ID
+    tempDat = csvReader[csvReader['call-id']==idKey]
+    
+    #record start and end times for the entire call
+    startTime = min(tempDat[tempDat.columns[2]])
+    endTime = max(tempDat[tempDat.columns[3]])
+    
+        #record call id, start/end time
+    idNest['call-id'] = idKey
+    idNest['start']=startTime
+    idNest['end']=endTime
+    
+    #find unique users in each call and iterate through each
+    user = tempDat[tempDat.columns[1]].unique()
+    for userKey in user:
+        #record data for specific user on call
+        userRow = tempDat[tempDat['user']==userKey]
+        userRow = userRow.drop('call-id',axis = 1)
+        userRowList = dict(userRow.iloc[0])
+        #append the existing list of users on call
+        idNest['users'].append(userRowList)
+    
+    #update Master List    
+    callList.append(idNest)
+    
+    
+#%% 
+#Write our data to .json file
+jsonFilename = 'jConvert_'+csvFilename[:-4]+'.json'
+with open(jsonFilename,'w') as jsonFile:
+    jsonFile.write(json.dumps(callList))
+  #%%  
+    
+    
+    
+    
+    
+    
+    
+    
+#Write JSON file
