eb9a50d19052c840ef418a7b7dfdef6ccd2532b8
James Moore <jmoore17@luc.edu>
2019-03-07 21:26:41 -0600

    Directions for running script added.

--- csvToJson_script.py
+++ csvToJson_script.py
@@ -3,6 +3,13 @@
 Created on Tue March 5 09:52:01 2019
 
 @author: James Moore
+Run this script on either 'calls-4.csv' or 'calls-50.csv'
+Upon Runng the user will specify a directory. This is the directory containing
+    the 'calls...' files.
+Once the directory is specified the user will input either the 'calls-4.csv'
+    or 'calls-50.csv' file.
+Finally, the script will take this file and convert it into a .JSON file.
+The .json file will be written to the working directory of the form 'jConvert...'
 """
 
 # Import appropriate libraries
@@ -11,80 +18,80 @@ import os
 import sys
 import pandas as pd
 
-#specify directory
+# Specify directory
 wkdir = 'NA'
 csvFilename = 'NA'
 numLoop = 0
 while os.path.isdir(wkdir)==False or os.path.isfile(csvFilename)==False:
-    #specify directory of file to convert
+    # Specify directory of file to convert
     wkdir = str(input('Please Specify Directory: '))     
     if os.path.isdir(wkdir)==False:
         print('Invalid Directory Specified:\n')
         numLoop = numLoop+1
-    #specify name of file to convert
+    # Specify name of file to convert
     else:
         os.chdir(wkdir)
         csvFilename = str(input('Please Specify File Name: '))
         
-        #Check if the user does not enter a .csv specification
+        # Check if the user does not enter a .csv specification
         if csvFilename[-4:] != '.csv':
             csvFilename = csvFilename+'.csv'
         
-        #check if file exists
+        # Check if file exists
         if os.path.isfile(csvFilename)==False:
             print('\nInvalid Input:\n' \
                   'The file entered does not exist in the specified directory.\n' \
                   'Please Check Directory and File Specifications and Try Again.\n')
             numLoop = numLoop+1
     
-    #terminate after 3 failed attempts to find existing file        
+    # Terminate after 3 failed attempts to find existing file        
     if numLoop > 3:
         sys.exit('Too Many Failed Attempts\n' \
                  'Program Terminating...')
 #%%  
-#Read in .csv data using pandas
+# Read in .csv data using pandas
 csvReader = pd.read_csv(csvFilename)
 
-#create blank list to write to .json
+# Create blank list to write to .json
 callList = list()
 
-#get unique call-ids
+# Get unique call-ids
 callID = csvReader[csvReader.columns[0]].unique()
 
-#iterate through unique call-ids
+# Iterate through unique call-ids
 for idKey in callID:
-    #set up nested list
+    # Set up nested list
     nestIDList = ('call-id','start','end','users')
     idNest = dict([(key,[]) for key in nestIDList])
     
-    #subset based on call-ID
+    # Subset based on call-ID
     tempDat = csvReader[csvReader['call-id']==idKey]
     
-    #record start and end times for the entire call
+    # Record start and end times for the entire call
     startTime = min(tempDat[tempDat.columns[2]])
     endTime = max(tempDat[tempDat.columns[3]])
     
-        #record call id, start/end time
+    # Record call id, start/end time
     idNest['call-id'] = idKey
     idNest['start']=startTime
     idNest['end']=endTime
     
-    #find unique users in each call and iterate through each
+    # Find unique users in each call and iterate through each
     user = tempDat[tempDat.columns[1]].unique()
     for userKey in user:
-        #record data for specific user on call
+        # Record data for specific user on call
         userRow = tempDat[tempDat['user']==userKey]
         userRow = userRow.drop('call-id',axis = 1)
         userRowList = dict(userRow.iloc[0])
-        #append the existing list of users on call
+        # Append the existing list of users on call
         idNest['users'].append(userRowList)
     
-    #update Master List    
+    # Update Master List    
     callList.append(idNest)
     
     
 #%% 
-#Write our data to .json file
+# Write our data to .json file
 jsonFilename = 'jConvert_'+csvFilename[:-4]+'.json'
 with open(jsonFilename,'w') as jsonFile:
     jsonFile.write(json.dumps(callList))
--- validation_Script.py
+++ validation_Script.py
@@ -3,7 +3,13 @@
 Created on Wed March 6 09:24:41 2019
 
 @author: James Moore
+
+Run this script on any of the 'calls...' files in the repository. 
+For 'calls-4.csv' and 'calls-50.csv' the script will pass.
+For 'calls-2k.dirty.csv' the script will print out a series of issues and 
+    tell the user that the file is corrupt with improper encoding in line 21060.
 """
+
 
 # Import appropriate libraries
 import csv
@@ -12,41 +18,41 @@ import sys
 import re
 #%%
 
-#specify directory
+# Specify directory
 wkdir = 'NA'
 csvFilename = 'NA'
 numLoop = 0
 while os.path.isdir(wkdir)==False or os.path.isfile(csvFilename)==False:
-    #specify directory of file to convert
+    # Specify directory of file to convert
     wkdir = str(input('Please Specify Directory: '))     
     if os.path.isdir(wkdir)==False:
         print('Invalid Directory Specified:\n')
         numLoop = numLoop+1
-    #specify name of file to convert
+    # Specify name of file to convert
     else:
         os.chdir(wkdir)
         csvFilename = str(input('Please Specify File Name: '))
         
-        #Check if the user does not enter a .csv specification
+        # Check if the user does not enter a .csv specification
         if csvFilename[-4:] != '.csv':
             csvFilename = csvFilename+'.csv'
         
-        #check if file exists
+        # Check if file exists
         if os.path.isfile(csvFilename)==False:
             print('\nInvalid Input:\n' \
                   'The file entered does not exist in the specified directory.\n' \
                   'Please Check Directory and File Specifications and Try Again.\n')
             numLoop = numLoop+1
     
-    #terminate after 3 failed attempts to find existing file        
+    # Terminate after 3 failed attempts to find existing file        
     if numLoop > 3:
         sys.exit('Too Many Failed Attempts\n' \
                  'Program Terminating...')
 
 #%%
-##Define Checking functions for each feature
+## Define Checking functions for each feature
         
-#Define Function for checking ID format
+# Define Function for checking ID format
 def id_Checker(matchString):
     pattern = re.compile('\w{8}-\w{4}-\w{4}-\w{4}-\w{12}')
     matchString = str(matchString)
@@ -60,7 +66,7 @@ def id_Checker(matchString):
     except:
         return False
 
-#Define Function for checking time format
+# Define Function for checking time format
 def time_Checker(matchString):
     pattern = re.compile('[12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])[T]'\
                          '([01]?\d|2[0-3]):([0-5]?\d):?([0-5]?\d)[.][0]{3}[Z]')
@@ -75,7 +81,7 @@ def time_Checker(matchString):
     except:
         return False
 
-#Define Function for Checking User format  
+# Define Function for Checking User format  
 def user_Checker(matchString):
     matchString = str(matchString[-4:])
     pattern = re.compile('[a-z]\d{3}')
@@ -90,47 +96,47 @@ def user_Checker(matchString):
         return False
     
 #%%
-#Report Invalidities and their corresponding locations
+# Report Invalidities and their corresponding locations
 numErrors = 0
 with open(csvFilename) as csvFile:
     csvReader = csv.DictReader(csvFile)
-    #Try to iterate through each row of file
+    # Try to iterate through each row of file
     try:    
         for row in csvReader:
-            #check if there are more than 5 fields in data
+            # Check if there are more than 5 fields in data
             if len(row)>5:
                 print("Error with Entry: "+str(csvReader.line_num)+". Too many fields detected.")
                 numErrors +=1
-            #check if there are less than 5 fields in data
+            # Check if there are less than 5 fields in data
             if len(row)<5:
                 print("Error with Entry: "+str(csvReader.line_num)+". Not all fields are detected.")
                 numErrors +=1
-            #check if call-id and media-id follow proper format
+            # Check if call-id and media-id follow proper format
             if id_Checker(row['call-id'])==False:
                 print("Error with Entry: "+str(csvReader.line_num)+". Data does not follow call-id format.")
                 numErrors += 1
             if id_Checker(row['media-id'])==False:
                 print("Error with Entry: "+str(csvReader.line_num)+". Data does not follow media-id format.")
                 numErrors += 1   
-            #check if Time follows proper format
+            # Check if Time follows proper format
             if time_Checker(row['start'])==False:
                 print("Error with Entry: "+str(csvReader.line_num)+". Data does not follow start time format.")
                 numErrors +=1
             if time_Checker(row['end'])==False:
                 print("Error with Entry: "+str(csvReader.line_num)+". Data does not follow end time format.")
                 numErrors +=1
-            #Check Valid User Format
+            # Check Valid User Format
             if user_Checker(row['user'])==False:
                 print("Error with Entry: "+str(csvReader.line_num)+". Data does not follow user format.")
                 numErrors +=1                
-    #Exception if row is unable to be read
+    # Exception if row is unable to be read
     except:
         numErrors += 1
         print("\nUnable to Encode Entry: "+str(csvReader.line_num+1)+". Abnormal encoding type at this location.")
         sys.exit('Terminating Program...')
         
 
-#Let user know if file passes validation script
+# Let user know if file passes validation script
 if numErrors == 0:
     print('File Passes Validation') 
 #%%
